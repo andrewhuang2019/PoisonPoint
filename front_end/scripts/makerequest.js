9c68620712d014b82884eba4010ef2cee3189b42
Andysword <andrewhuang2019@gmail.com>
2024-04-13 11:49:16 -0500

    Updated makerequest.js
    Added extra functions that utilize promises with fetch functions to send data

--- front_end/scripts/makerequest.js
+++ front_end/scripts/makerequest.js
@@ -10,15 +10,61 @@ function add_to_database(data){
             headers: {
                 'Content-Type': 'application/json'
             },
+            body: JSON.stringify({key: data})
+        }, )
+        .then(response => response.json())
+        .then(data => {
+            resolve("Success!");
+        })
+        .catch((error) => {
+            reject("Error!");
+        })
+
+    })
+
+}
+
+function get_from_database(data){
+
+    return new Promise((resolve, reject) => {
+
+        fetch('local-url', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify(data)
         }, )
-
-        const condition = true;
-        if (condition){
+        .then(response => response.json())
+        .then(data => {
             resolve("Success!");
-        } else {
+        })
+        .catch((error) => {
             reject("Error!");
-        }
+        })
+
+    })
+
+}
+
+function remove_from_database(data){
+
+    return new Promise((resolve, reject) => {
+
+        fetch('local-url', {
+            method: 'delete',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({key: data})
+        }, )
+        .then(response => response.json())
+        .then(data => {
+            resolve("Success!");
+        })
+        .catch((error) => {
+            reject("Error!");
+        })
 
     })
 
