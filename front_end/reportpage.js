cecd27570adc53bd2095aa59a7e912589d4c196b
Andysword <andrewhuang2019@gmail.com>
2024-04-13 02:03:31 -0500

    Updated create_new_location
    updated the options tag to be taken from the global array "updated_array"

--- front_end/reportpage.js
+++ front_end/reportpage.js
@@ -1,11 +1,9 @@
 
 var num = 0;
-var updated_array = [];
+var updated_array = ["Pizza Hut", "Chipotle", "McDonalds"];
 var removed_array = [];
 
 function create_new_location(){
-
-    
 
     //creates a new select tag in the html document
     var new_select = document.createElement('select');
@@ -15,14 +13,21 @@ function create_new_location(){
     new_select.name = "location" + String(num);
 
     //creates a new option tag in the html document
-    var option = document.createElement('option');
+
 
     //sets the option tag attributes for the text content and its value
-    option.textContent = "Pizza Hut";
-    option.value = option.textContent;
+    for (let num in updated_array){
+        var option = document.createElement('option');
+        option.textContent = updated_array[num];
+        option.value = option.textContent;
+        new_select.appendChild(option);
+
+    }
+    //option.textContent = "Pizza Hut";
+    //option.value = option.textContent;
 
     //appends the option tag to the select tag as a child (contained within)
-    new_select.appendChild(option);
+    //new_select.appendChild(option);
     
     //appends the select tag to the tag with the location_division id
     document.getElementById('location_division').appendChild(new_select);
@@ -31,6 +36,8 @@ function create_new_location(){
     new_time.type = "date";
     new_time.name = "time" + String(num);
 
+    document.getElementById('location_division').appendChild(new_time);
+
     //appends a break to the tag with the location_division id
     document.getElementById('location_division').appendChild(document.createElement('br'));
 
@@ -38,3 +45,10 @@ function create_new_location(){
 
 }
 
+function store_array(restaurants){
+
+    for(let restaurant in restaurants){
+        updated_array.push(restaurant);
+    }
+
+}
