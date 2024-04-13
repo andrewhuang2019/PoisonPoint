dfb8e26ea87042a430dcfb260867a6b9ebd7ea7f
Keaton Xu <keatx@ku.edu>
2024-04-13 16:35:23 -0500

    Update report html and js

--- front_end/reportpage.js
+++ front_end/reportpage.js
@@ -97,3 +97,39 @@ function update_restaurant_array(){
 function add_restaurant_array(){
 
 }
+
+let autocomplete;
+function initAutocomplete(){
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'),
+        {
+        types: ['establishment'],
+        componentRestrictions: {'country':['US']},
+        fields: ['place_id','geometry','name']
+    });
+    autocomplete.addListener('place_changed',onPlaceChanged);
+}
+
+function onPlaceChanged(){
+    var place = autocomplete.getPlace();
+
+    if (!place.geometry){
+        document.getElementById('autocomplete').placeholder = 'Enter a restaurant';
+    } else{
+        var data = {
+            "name": place.name,
+            "ID": place.place_id,
+            "location": place.geometry
+        };
+        
+        var jsonData = JSON.stringify(data);
+
+        fetch('/process', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            },
+            body: jsonData
+        })
+    }
+}
